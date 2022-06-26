ff4b9180f41d72aea048e0f3133ce4012797deec
Patrik83 <patrik.norrgard@yahoo.se>
2022-06-26 14:17:22 +0200

    Add files via upload

---
+++ project_notifications/INotificationHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace project_notifications
+{
+    // Delat gränssnitt för Android och iOS
+    public interface INotificationHandler
+    {
+        void SendNotification(string title, string message);
+    }
+}
---
+++ project_notifications/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace project_notifications
+{
+    public partial class MainPage : ContentPage
+    {
+        INotificationHandler notification;
+
+        DateTime _triggerTime;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // Initiera referensen till gränssnittet för att kunna utföra meddelandet
+            notification = DependencyService.Get<INotificationHandler>();
+            // Ansvarar för att påbörja utförandet av meddelandet enligt schemaläggningen
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
+        {
+            if (_switch.IsToggled)
+            {
+                if (DateTime.Now >= _triggerTime)
+                {
+                    _switch.IsToggled = false; // Inaktiverad för att skapa ytterligare ett meddelande
+                    notification.SendNotification("Min app", "Glöm inte bort att röra på dig!");
+                }
+            }
+            return true;
+        }
+
+        // Metod som ansvarar för att fånga tiden för TimePicker-elementet x:Name="firstTime"
+        void OnTimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+            }
+        }
+
+        // Metod som BESTÄMMER eller STÄLLER in körningstiden baserat på OnTimePickerPropertyChanged PROPERTY för TimePicker-elementet x:Name="timePicker"
+        void SetTriggerTime()
+        {
+            if (_switch.IsToggled)
+            {
+                _triggerTime = DateTime.Today + firstTime.Time;
+
+                if (_triggerTime < DateTime.Now)
+                {
+                    _triggerTime += TimeSpan.FromDays(1);
+                }
+            }
+        }
+
+        // Fångar värdet på Toggled-egenskapen för Switch-elementet x:Name="_switchActive"
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+        }
+    }
+}
